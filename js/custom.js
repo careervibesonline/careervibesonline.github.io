2232cf5f9bd57fa5cf3dd3a64a785182dc0c0f65
inerudite <88968574+inerudite@users.noreply.github.com>
2022-01-22 17:17:23 +0530

    Slider 3

--- js/custom.js
+++ js/custom.js
@@ -371,6 +371,8 @@ $(function () {
 	$('.custom_sld_wrap').slick({
 		infinite: true,
 		autoplay: true,
+		prevArrow: $('.cust_sld_prev')
+		nextArrow: $('..cust_sld_next')
         autoplaySpeed: 2000,
 		slidesToShow: 1,
 		slidesToScroll: 1,
